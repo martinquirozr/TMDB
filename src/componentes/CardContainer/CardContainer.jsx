a93cc40d4214b8b53f2d77a71ea2d67cc0423efc
Martin Quiroz <martinquiroz545@gmail.com>
2024-05-07 13:39:53 -0600

    Nuevo Archivo

---
+++ src/componentes/CardContainer/CardContainer.jsx
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from '../Card/Card'
+
+function CardContainer(props){
+
+return(
+
+<Card>Hola</Card>
+
+
+    )
+
+}
+
+
+export default CardContainer;

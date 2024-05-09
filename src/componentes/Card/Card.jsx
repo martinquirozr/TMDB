68b8316601144d9aea5a3c18a4f73bdfa378cbd1
Martin Quiroz <martinquiroz545@gmail.com>
2024-05-09 14:54:25 -0600

    Corrección de errores

--- src/componentes/Card/Card.jsx
+++ src/componentes/Card/Card.jsx
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function Card(props){
 
 return(
-    <div className="col-6 +.6col-md-2 col-sm-4 mb-3 text-light" onClick={() => props.clickf(props.movie)}>
+    <div className="col-6 col-md-2 col-sm-4 mb-3 text-light" onClick={() => props.clickf(props.movie)}>
       <img className='rounded-3' src={`https://image.tmdb.org/t/p/original/${props.imgsrc}`} alt={props.titulo} height='auto' width='100%'/>
       <h6 className="text-center mt-2">{props.titulo}</h6>
     </div>
